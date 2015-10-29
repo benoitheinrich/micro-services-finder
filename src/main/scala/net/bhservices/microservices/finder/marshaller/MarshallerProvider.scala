3668e0d227f5bfb25c92e53a2f6165c09a0d5382
Benoit Heinrich <benoit.heinrich@vocado.com>
2015-10-29 02:47:15 +0100

    #4: Removed `T <: AnyRef` from MarshallerProvider. #4: Added missing doc.

--- src/main/scala/net/bhservices/microservices/finder/marshaller/MarshallerProvider.scala
+++ src/main/scala/net/bhservices/microservices/finder/marshaller/MarshallerProvider.scala
@@ -3,9 +3,9 @@ package net.bhservices.microservices.finder.marshaller
 import scala.reflect.Manifest
 
 /**
- *
+ * A provider for Marshalling some data to string and from string.
  */
-trait MarshallerProvider[T <: AnyRef] {
+trait MarshallerProvider[T] {
   def marshaller(implicit m: Manifest[T]): Marshaller
 
   trait Marshaller {
